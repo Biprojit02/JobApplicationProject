35fc7ce657aa5e5684137ff3217b92b68ce88d1c
Biprojit03 <biprojitgiri@y4elabs.com>
2025-04-04 15:40:07 +0530

    Updated Job Application Project with new features/fixes

--- src/main/java/com/bipro/JobApplicationProject/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/bipro/JobApplicationProject/review/impl/ReviewServiceImpl.java
@@ -1,6 +1,8 @@
 package com.bipro.JobApplicationProject.review.impl;
 
 import com.bipro.JobApplicationProject.review.ReviewService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReviewServiceImpl implements ReviewService {
 }
